a4c4b10e3327c165c1304e33fab5f4b21606695b
eliao13 <eliao13@my.bcit.ca>
2022-06-16 21:19:29 -0700

    Added Link for nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,23 @@
+import {Link} from "react-router-dom";
+import About from "./About"
+import Contact from "./Contact"
+import Projects from "./Projects"
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <ul>
+        <li>
+          <Link to={About} />
+        </li>
+        <li>
+          <Link to={Projects} />
+        </li>
+        <li>
+          <Link to={Contact} />
+        </li>
+      </ul>
+    </div>
   )
 }
 export default Navbar
