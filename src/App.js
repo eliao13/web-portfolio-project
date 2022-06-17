db1fb2c67ade1a18f439db3e78f1d46f20274d47
Evan <eliao13@my.bcit.ca>
2022-06-17 15:49:14 -0700

    added links to sections and some css

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-      <header className="App-header">
-      </header>
-      <Navbar /> 
-      <About /> 
-      <Projects />
-      <Contact />
-    </main>
+    <BrowserRouter>
+      <div className="site-wrapper">
+        <main className='site-main'>
+          <header />
+          <Navbar />
+          <About />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,43 @@
-import {Link} from "react-router-dom";
-import About from "./About"
-import Contact from "./Contact"
-import Projects from "./Projects"
+import { HashLink as Link } from "react-router-hash-link";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+
+  const [prevScrollPosition, setPrevScrollPosition] = useState(0);
+
+  const [visible, setVisible] = useState(true);
+
+  function handleScroll() {
+
+    // Find current scroll position 
+    const currentScrollPosition = window.pageYOffset;
+
+    // Set state based on location info
+    setVisible((prevScrollPosition > currentScrollPosition && prevScrollPosition - currentScrollPosition > 70) || currentScrollPosition < 10);
+
+    setPrevScrollPosition(currentScrollPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPosition, visible, handleScroll]);
+
   return (
-    <div>
+    <div className="nav-bar">
       <ul>
         <li>
-          <Link to={About} />
+          <Link smooth to='/' className="home-link">Home</Link>
         </li>
         <li>
-          <Link to={Projects} />
+          <Link smooth to='#about' className="about-link">About</Link>
         </li>
         <li>
-          <Link to={Contact} />
+          <Link smooth to='#projects' className="projects-link">Projects</Link>
+        </li>
+        <li>
+          <Link smooth to='#contact' className="contact-link">Contact</Link>
         </li>
       </ul>
     </div>
