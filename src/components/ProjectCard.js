ad5fb39439a2baffd0440dde9ab1d52b03a4906e
eliao13 <eliao13@my.bcit.ca>
2022-06-23 22:54:50 -0700

    added card carousel for projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,26 @@
-function ProjectCard({title, description, image, link}) {
+import Skills from "./Skills";
+
+function ProjectCard({className, title, shortSummary, image, link, singleProject}) {
+
   return (
-    <div className="projectCard">
+    <div className={className}>
         <img src={image} alt={`screen shot of ${title}`} />
         
         <h2>{title}</h2>
 
-        <p>{description}</p>
+        <ul>
+          {singleProject.map((detail, i) => {
+            return (
+             <Skills detail={detail}
+                           key={i}
+                          />
+            )
+          })}
+        </ul>
+
+        <p>{shortSummary}</p>
 
         <a className="project-card-links" href={link}>Live Site</a>
-
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,76 @@
+import Slider from "react-slick";
 import projectsData from "../data/projects-data";
 import ProjectCard from "./ProjectCard";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { useState } from "react";
 
 function Projects() {
+
+
+  // Card Carousel - https://react-slick.neostack.com/docs/example/responsive
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    focusOnSelect: true,
+    infinite: true,
+    lazyLoad: true,
+    speed: 1000,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    autoplay: true, 
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    swipeToSlide: true,
+
+    // changing image index when clicking either arrow to be the previous or next index
+    beforeChange: (current, next) => setImageIndex(next),
+
+    responsive: [
+      { 
+        breakpoint: 1000, 
+        settings: { 
+          slidesToShow: 1, 
+        }
+      }
+    ]
+  };
 
   return (
     <section className="projects" id="projects">
       <h1>My Projects</h1>
 
-      <div className="project-cards">
+      <Slider {...settings}>
         {projectsData.map((project, i) => {
-          return <ProjectCard key={i}
+          return (<ProjectCard key={i}
+                              className={i === imageIndex ? 'slide activeSlide' : 'slide'}
                               title={project.title}
-                              description={project.description}
-                              image={project.image}
-                              link={project.link} />
+                              shortSummary={project.shortSummary}
+                              image={project.sampleImage}
+                              link={project.link} 
+                              singleProject={project.singleProject}
+                              />)
         })}
-      </div>
+      </Slider>
     </section>
   );
 }
