73cfaf88ec21a8c6ef2c04963e48a08a3f4aea0e
Evan <eliao13@my.bcit.ca>
2022-06-27 14:57:08 -0700

    stlyed herobanner

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -1,5 +1,5 @@
 import { HashLink as Link } from "react-router-hash-link";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 function Herobanner() {
 
@@ -7,7 +7,7 @@ function Herobanner() {
   const [text, setText] = useState("");
 
   // Final text we want to see typed out
-  const [fullText, setFullText] = useState("Hello, I'm Evan Liao. Welcome to my page."); 
+  const [fullText, setFullText] = useState("Hello, I'm Evan Liao.\nWelcome to my page.");
 
   // Current index of the displayed text
   const [index, setIndex] = useState(0);
@@ -17,7 +17,7 @@ function Herobanner() {
       setTimeout(() => {
         setText(text + fullText[index]);
         setIndex(index + 1);
-      }, 50)
+      }, 60)
     }
   }, [index]);
 
@@ -26,7 +26,7 @@ function Herobanner() {
       <h1>{text}</h1>
 
       <h2>
-        I'm a web-developer who is experienced in React and SASS. I also love to build project.
+        I'm a web-developer who is experienced in React and SASS. I also love to build projects.
       </h2>
       <Link smooth to='#about' className="about-link">More About Me</Link>
     </section>
