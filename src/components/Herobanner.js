4fde82b17b096ce1f46d9dcbadd2224067b6a076
Evan <eliao13@my.bcit.ca>
2022-06-27 09:30:39 -0700

    herbanner basics

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -1,6 +1,16 @@
+import { HashLink as Link } from "react-router-hash-link";
+
 function Herobanner() {
   return (
-    <div>Herobanner</div>
+    <section>
+      <h1>
+        Hello, I'm Evan Liao.<br />Welcome to my page.
+      </h1>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, quos consequuntur. Sint distinctio fugiat minima eveniet libero, odit, ipsa id unde ex vero asperiores, pariatur sed dolore eaque quasi hic.
+      </h2>
+      <Link smooth to='#about' className="about-link">More About Me</Link>
+    </section>
   )
 }
 export default Herobanner
