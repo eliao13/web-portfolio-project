ed05af57d5274ec8a22bdc0d04f6b1de45d69d0a
eliao13 <eliao13@my.bcit.ca>
2022-06-27 11:28:03 -0700

    created typing animation

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -1,16 +1,35 @@
 import { HashLink as Link } from "react-router-hash-link";
+import {useState, useEffect} from "react";
 
 function Herobanner() {
+
+  // Current text displayed on the page
+  const [text, setText] = useState("");
+
+  // Final text we want to see typed out
+  const [fullText, setFullText] = useState("Hello, I'm Evan Liao. Welcome to my page."); 
+
+  // Current index of the displayed text
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < fullText.length) {
+      setTimeout(() => {
+        setText(text + fullText[index]);
+        setIndex(index + 1);
+      }, 50)
+    }
+  }, [index]);
+
   return (
-    <section>
-      <h1>
-        Hello, I'm Evan Liao.<br />Welcome to my page.
-      </h1>
+    <section className="hero-banner">
+      <h1>{text}</h1>
+
       <h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, quos consequuntur. Sint distinctio fugiat minima eveniet libero, odit, ipsa id unde ex vero asperiores, pariatur sed dolore eaque quasi hic.
+        I'm a web-developer who is experienced in React and SASS. I also love to build project.
       </h2>
       <Link smooth to='#about' className="about-link">More About Me</Link>
     </section>
   )
 }
-export default Herobanner
+export default Herobanner;
