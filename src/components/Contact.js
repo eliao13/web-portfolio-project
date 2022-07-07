8d76a6aa0f928bad754fe67fc74d9a17acf8095d
eliao13 <eliao13@my.bcit.ca>
2022-07-06 17:19:07 -0700

    finished contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,23 @@
-import { SiLinkedin, SiGithub } from 'react-icons/si';
+import { SiLinkedin, SiGithub } from "react-icons/si";
 
 function Contact() {
   return (
     <section className="contact" id="contact">
-      <h2>Like what you have seen so far? <br />I would love to be in touch with you!</h2>
+      <h2>
+        Like what you see? <br />I would love to get connected with you!
+      </h2>
       <div className="social-media-icons">
-        <a className='social-media-icon' href="https://www.linkedin.com/in/evanwliao/"><SiLinkedin /></a>
-        <a className='social-media-icon' href="https://github.com/eliao13"><SiGithub /></a>
+        <a
+          className="social-media-icon"
+          href="https://www.linkedin.com/in/evanwliao/"
+        >
+          <SiLinkedin />
+        </a>
+        <a className="social-media-icon" href="https://github.com/eliao13">
+          <SiGithub />
+        </a>
       </div>
     </section>
-  )
+  );
 }
-export default Contact
+export default Contact;
