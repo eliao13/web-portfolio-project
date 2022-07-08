1043381b94f4e9107d79bc42561e73a41bba14bc
eliao13 <eliao13@my.bcit.ca>
2022-07-07 22:24:07 -0700

    finished project content

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -69,7 +69,7 @@ const projectsData = [
     shortSummary:
       "The Capstone Project was to setup a WooCommerce store where users can shop for outdoor equipment, such as hiking shoes, tents, and sleeping bags.",
     sampleImage: "/assets/image/capstone-sample-image.jpg",
-    link: "https://www.evanwliao.com/capstone-project/",
+    link: "https://getoutdoors.bcitwebdeveloper.ca/",
     singleProject: [
       {
         title: "Capstone Project",
@@ -79,42 +79,45 @@ const projectsData = [
         timeline: "1 month",
         team: "Collaboration of 4 team members",
         role: "Developer/designer",
-        liveSiteLink: "https://www.evanwliao.com/capstone-project/",
+        liveSiteLink: "https://getoutdoors.bcitwebdeveloper.ca/",
         githubLink: "https://github.com/",
         overview:
           "Get Outdoors was a WooCommerce store that sells outdoor equipment for various outdoor activities and provides workshops on outdoor related topics. Goal of the capstone project was to develop an online store for Get Outdoors, where users were able to perform various actions, such as view store details, view workshop details, purchase products and ship it to Canada or USA or pick up locally.",
         development: [
           {
-            title: "Process",
+            title: "Planning",
             content:
               "This capstone project was a team project that involved a bigger planning process compared to other projects in the BCIT program. Part of this process was creating several documentations that included memorandum of understanding, project management & communication plan, website content plan & information architecture, wireframe, and site development plan.",
-            screenshot: "",
+            screenshot: "/assets/image/capstone-site-map.jpg",
           },
           {
-            title: "Functional Website",
+            title: "Development Process",
             content:
-              "The process of building a functional website involved implementing all the plugins that our team planned to use and outputting the plugins correctly onto the web application. The process also involved checking for proper semantics for every element in the web application.",
-            screenshot: "",
-          },
-          {
-            title: "Styling",
-            content:
-              "The styling process involved checking for reused components and splitting the tasks into reused components. By splitting the tasks into reused components allows for consistency and eliminates any redundant work.",
+              "The development process for this capstone project was separated into functional website and styling. Building a functional website involved implementing all the plugins that our team planned to use and outputting the plugins correctly onto the web application. The process of building a functional website also involved checking for proper semantics for every element in the web application and making sure all the functions work. Styling part of this project involved splitting the tasks into reuseable parts and all the other parts.",
           },
           {
             title: "Takeaways",
             content:
-              "The styling process involved checking for reused components and splitting the tasks into reused components. By splitting the tasks into reused components allows for consistency and eliminates any redundant work.",
+              "This capstone project was fun and I gained great experience working in a team environment. I learned a lot working in a team environment including collaborating using project management tool, having daily meetings, and having effective communications within the team.",
           },
         ],
-        design: [{}],
+        design: [
+          {
+            title: "Capstone project wireframe",
+            screenshot: "/assets/image/capstone-wireframe-ss.jpg",
+          },
+          {
+            title: "Capstone project wireframe",
+            screenshot: "/assets/image/capstone-wireframe-ss-2.jpg",
+          },
+        ],
       },
     ],
   },
   {
     title: "Portfolio Project",
     shortSummary:
-      "Portfolio project was developed to showcase my skills as a developer. This project was developed using React and Sass.",
+      "A personal portfolio that showcases my skills, my front end web developer journey, and my previous works.",
     sampleImage: "/assets/image/portfolio-sample-image.jpg",
     link: "https://www.evanwliao.com/",
     singleProject: [
@@ -129,30 +132,30 @@ const projectsData = [
         liveSiteLink: "https://www.evanwliao.com/",
         githubLink: "https://github.com/",
         overview:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla orci eu mauris luctus, vitae tincidunt quam porttitor. Donec vitae nisi quis metus tincidunt auctor eu vel orci. Pellentesque viverra est at erat vulputate, eu rhoncus nisi pharetra. Donec arcu quam, venenatis vel erat vel, rhoncus consequat ligula. Quisque non eleifend libero, varius aliquet enim. Nunc et consequat velit, sit amet semper elit. Vestibulum in mi ac metus iaculis volutpat. Sed magna eros, aliquam at purus sit amet, fringilla suscipit ligula. Donec tincidunt orci et odio tincidunt finibus.",
+          "Portfolio project was developed to showcase my skills as a developer. This project was developed using React and Sass. Wireframes were done through Figma and high fidelity mockup was done through Adobe XD.",
         development: [
           {
-            title: "Process",
+            title: "Planning",
             content:
-              "This capstone project was a team project that involved a bigger planning process compared to other projects in the BCIT program. Part of this process was creating several documentations that included memorandum of understanding, project management & communication plan, website content plan & information architecture, wireframe, and site development plan.",
-            screenshot: "",
+              "Before starting the development of the portfolio, a content plan was made to layout all the content that is going to be included in each section of the web application. The content plan helped me stay organized in the development process.",
+            screenshot: "/assets/image/portfolio-content-plan.jpg",
           },
           {
-            title: "Functional Website",
+            title: "Accordion Feature",
             content:
-              "The process of building a functional website involved implementing all the plugins that our team planned to use and outputting the plugins correctly onto the web application. The process also involved checking for proper semantics for every element in the web application.",
-            screenshot: "",
+              "Through implementing the accordion feature, I learned how to use and customize React libraries, such as Material UI. Using accordions allow me to display more content, while not having the page extend too long.",
+            screenshot: "/assets/image/portfolio-accordion.gif",
           },
           {
-            title: "Styling",
+            title: "Moveable Cards",
             content:
-              "The styling process involved checking for reused components and splitting the tasks into reused components. By splitting the tasks into reused components allows for consistency and eliminates any redundant work.",
-            screenshot: "",
+              "Moveable cards allows interaction between the user and the web application. The cards design is also an interesting and fun way of presenting content.",
+            screenshot: "/assets/image/portfolio-card.gif",
           },
           {
             title: "Takeaways",
             content:
-              "The styling process involved checking for reused components and splitting the tasks into reused components. By splitting the tasks into reused components allows for consistency and eliminates any redundant work.",
+              "Overall, I had fun making the portfolio project. I wanted to design my portfolio to mimick writing on a notebook. I also gained a lot of experience in React and SASS while developing the portfolio, including implementing different React libraries and customizing the components to fit my theme. ",
           },
         ],
         design: [
